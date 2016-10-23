aa40bcc1fe024a669a1d50397aee0852606b49be
Albert Chen <albert@adaptrum.com>
2016-10-23 16:54:12 -0700

    this is the actual first commit

---
+++ HelloWorld/Global.asax.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace HelloWorld
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+               "Process", "Process/{action}/{id}",
+               defaults: new
+               {
+                   controller = " Process",
+                   action = "List ",
+                   id =
+                  UrlParameter.Optional
+               });
+
+            routes.MapRoute(
+               name: "Default", url: "{controller}/{action}/{id}",
+               defaults: new
+               {
+                   controller = "Home",
+                   action = "Index",
+                   id =
+                  UrlParameter.Optional
+               });
+        }
+    }
+}
